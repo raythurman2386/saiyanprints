1ae582f174cc75cb399a796ff04fb9f9f0da9400
raythurman2386 <raymondthurman5@gmail.com>
2023-08-22 17:12:12 -0500

    set serve false to admin dashboard

--- saiyanprints-store/medusa-config.js
+++ saiyanprints-store/medusa-config.js
@@ -47,6 +47,7 @@ const plugins = [
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
       autoRebuild: false,
+      serve: false,
       develop: {
         open: process.env.OPEN_BROWSER !== "false",
       },
