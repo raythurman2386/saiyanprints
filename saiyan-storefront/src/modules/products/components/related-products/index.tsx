4d76ccd3fc2c065f1d6e21f58a803ff97a30a6f9
raythurman2386 <raymondthurman5@gmail.com>
2023-08-25 12:01:47 -0500

    featured products grid fixed

--- saiyan-storefront/src/modules/products/components/related-products/index.tsx
+++ saiyan-storefront/src/modules/products/components/related-products/index.tsx
@@ -58,9 +58,9 @@ const RelatedProducts = ({ product }: RelatedProductsProps) => {
         </p>
       </div>
 
-      <ul className="grid grid-cols-2 small:grid-cols-3 medium:grid-cols-4 gap-x-4 gap-y-8t">
+      <ul className="grid grid-cols-1 xsmall:grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 text-slate-900">
         {previews.map((p) => (
-          <li key={p.id}>
+          <li key={p.id} className="m-auto">
             <ProductPreview {...p} />
           </li>
         ))}
